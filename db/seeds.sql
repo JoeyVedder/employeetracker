636c7415e70cc4d97c827c3230559e13a5c163c8
joey vedder <qilotog1@gmail.com>
2024-09-18 23:53:29 -0400

    added some TABLEs

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO manager_id (first_name, last_name, role_id, manager_id)
+VALUES ('')
+
+
+INSERT INTO department (name)
+VALUES ('Web Development'),
+       ('Robotics'),
+       ('Game Development'),
+       ('Aerospace Engineer'),
+       ('IT'),
+       ('Data Science'),
+       ('Cybersecurity');
